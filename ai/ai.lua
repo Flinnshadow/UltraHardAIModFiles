01a968ae49d27db4e420a9ba8b3f1a44e9fa2d07
Cronkhinator <cronkhinator.coc@gmail.com>
2025-02-15 13:39:20 +0100

    Fixed some building errors

--- ai/ai.lua
+++ ai/ai.lua
@@ -2677,7 +2677,7 @@ function OnDeviceCompleted(ODCteamId, deviceId, saveName)
             else
                actualNodeB = nil
                LogError("CreateNode failed: " .. CS[newNodeId] .. " from actual N" .. actualNodeA)
-               if newNodeId == CS_NODESTOOFAR or newNodeId == CS_NODESTOOCLOSE or newNodeId == CS_NOGROUND then
+               if newNodeId == CS_NODESTOOCLOSE or newNodeId == CS_NOGROUND or newNodeId == CS_NODESTOOFAR and Vec3Dist(NodePosition(actualNodeA), dest) < GetMaterialMaxLength(action.MaterialSaveName, teamId) + LinkStretch then
                   return CreateNodeDynamic(newNodeId, action, actualNodeA, dest, index)
                else
                   if ShowPositions then
